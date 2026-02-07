b52ac54f225808c99c8409e00508599ebe1cf154
deathcoding <ilgiz.ilishev@gmail.com>
2026-02-07 15:18:47 +0500

    readme update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./app/App.jsx";
 import { StoreProvider } from "./app/providers/StoreProvider.jsx";
 
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <StoreProvider>
